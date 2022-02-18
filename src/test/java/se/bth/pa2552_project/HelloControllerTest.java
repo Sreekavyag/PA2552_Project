247fe196df4407dba04a13eb6f50d8af1b344897
Stephanie Sundqvist Tepfers <stephaniest1@gmail.com>
2022-02-18 13:55:41 +0100

    Add basic test.

---
+++ src/test/java/se/bth/pa2552_project/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package se.bth.pa2552_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void onHelloButtonClick() {
+    }
+}
