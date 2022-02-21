e9ffba5669a936b9b97ab88cecfac18d81ebbd6b
Stephanie Sundqvist Tepfers <stephaniest1@gmail.com>
2022-02-21 18:12:27 +0100

    Add basic system test example.

--- src/test/java/se/bth/pa2552_project/BankingSystemTest.java
+++ src/test/java/se/bth/pa2552_project/BankingSystemTest.java
@@ -1,10 +1,13 @@
 package se.bth.pa2552_project;
 
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
@@ -24,7 +27,15 @@ class BankingSystemTest {
     }
 
     @Test
-    public void testButtonClick() {
-        assertThat(true).isTrue();
+    public void testButtonClick(FxRobot robot) {
+        Button helloButton = robot.lookup("#helloButton").queryButton();
+        Label helloLabel = robot.lookup("#welcomeText").queryAs(Label.class);
+        assertThat(helloLabel.getText()).isEmpty();
+
+        robot.clickOn(helloButton);
+
+        assertThat(helloLabel.getText())
+                .isNotEmpty()
+                .isEqualTo("Welcome to JavaFX Application!");
     }
 }
